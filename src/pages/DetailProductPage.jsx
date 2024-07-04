1d7625c1639d3f8ed414782f2caab1a98ce1cacf
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-07-04 18:15:54 -0500

    fix view detail, add home image v0.3

--- src/pages/DetailProductPage.jsx
+++ src/pages/DetailProductPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, useNavigate } from 'react-router-dom'
 import { useCart } from '../hooks/useCart'
 // import './DetailProductPage.css'
 
@@ -9,18 +9,24 @@ export const DetailProductPage = () => {
   const { cart, addToCart, removeFromCart } = useCart()
   const isItemInCart = (product) => cart.some(item => item.id === product.id)
   const isInCart = isItemInCart(mappedProduct)
-  console.log(mappedProduct)
+  // console.log(mappedProduct)
+
+  const navigate = useNavigate()
+
+  const handleNavigateBack = () => {
+    navigate(-1)
+  }
 
   return (
-    <section className='detailProduct flex justify-center bg-zinc-950 items-center min-h-screen-dvh '>
-      <article className='detailProduct-card w-9/12 bg-zinc-900 flex flex-col lg:flex-row gap-10 px-20 py-12 rounded-md h-[80dvh] border border-zinc-800'>
-        <div className='flex gap-4 w-[60%]'>
-          <aside className='w-[20%]'>
-            <ul className=' grid grid-cols-1 grid-rows-4 h-fit justify-start gap-4'>
+    <section className='detailProduct flex justify-center bg-zinc-950 items-center min-h-screen-dvh py-4 lg:py-0'>
+      <article className='detailProduct-card w-10/12 lg:w-9/12 bg-zinc-900 flex flex-col items-center lg:flex-row gap-x-8 px-4 lg:px-[6%] rounded-md h-fit lg:h-[80dvh] border border-zinc-800 relative py-4 lg:py-0 lg:max-h-[700px]'>
+        <div className='flex flex-col-reverse gap-4 items-center lg:items-start lg:flex-row lg:gap-[2%] w-[85%] lg:w-[60%]'>
+          <aside className='lg:min-w-16 lg:w-[15%]'>
+            <ul className=' flex w-full lg:w-[initial] lg:grid grid-cols-1 grid-rows-4 lg:h-fit gap-4'>
               {mappedProduct.images.map((img, ind) => (
                 <li key={ind}>
                   <img
-                    className={`w-full aspect-[1/1] bg-zinc-400 rounded-md ${principalImg === img ? ' opacity-60' : ''}`}
+                    className={`w-20 lg:w-full aspect-[1/1] object-cover cursor-pointer bg-zinc-400 rounded-md ${principalImg === img ? ' opacity-60' : ''}`}
                     src={img}
                     alt={`Image of ${mappedProduct.name}`}
                     onClick={() => {
@@ -31,10 +37,10 @@ export const DetailProductPage = () => {
               ))}
             </ul>
           </aside>
-          <img src={principalImg} alt={`Image of ${mappedProduct.name}`} className='w-full aspect-[1/1] bg-zinc-400 rounded-md' />
+          <img src={principalImg} alt={`Image of ${mappedProduct.name}`} className='w-full lg:w-[86%] aspect-[1/1] bg-zinc-400 rounded-md object-cover' />
         </div>
-        <div className='detailProduct-info w-[40%] flex flex-col justify-around py-6 2xl:py-32'>
-          <h2 className='text-3xl'>{mappedProduct.name}</h2>
+        <div className='detailProduct-info w-[85%] lg:w-[40%] flex flex-col gap-3 2xl:gap-6 lg:h-full lg:justify-center pt-4 md:pt-0'>
+          <h2 className='text-2xl md:text-3xl font-bold'>{mappedProduct.name}</h2>
           <p className='text-xl'>{`$${mappedProduct.price}`}</p>
           <div>
             <span>Description:</span>
@@ -50,7 +56,11 @@ export const DetailProductPage = () => {
           >{isInCart ? 'Remove From Cart' : 'Add To Cart'}
           </button>
         </div>
-
+        <button
+          className=' absolute top-2 right-2 bg-zinc-950 px-2 py-1 rounded'
+          onClick={handleNavigateBack}
+        >Back
+        </button>
       </article>
     </section>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,22 @@
+import laptop from '../assets/img/laptopp.webp'
+
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <section className='principal-section min-h-screen-dvh grid grid-cols-1 md:grid-cols-2 gap-14 px-10 xl:px-28 py-7 xl:py-0 items-center'>
+        <div>
+          <h2 className=' text-4xl lg:text-5xl font-bold '>Power and performance with cutting-edge <span className='text-5xl lg:text-6xl font-bold text-cyan-600'>Technology</span></h2>
+          <p className='text-lg lg:text-xl mt-3 text-gray-400'>Work, play and create with maximum efficiency thanks to our high-performance laptops.</p>
+          <div className='flex gap-4 mt-3'>
+            <button className='px-4 py-2 bg-cyan-600 rounded font-bold'>Start Shoping</button>
+            <button className='px-4 py-2 bg-zinc-900 rounded font-bold'>About</button>
+          </div>
+        </div>
+        {/* <div className=' h-4/5 bg-[url("/src/assets/img/laptopp.webp")] bg-center bg-contain bg-no-repeat ' /> */}
+        <div>
+          <img src={laptop} alt='Laptop image' />
+        </div>
+      </section>
+    </>
   )
 }
