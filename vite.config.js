fb59bd0b2a5ea2581483dbc1b94b5fa0d065e23a
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-07-27 14:06:26 -0500

    git add meta data, robots v1.6

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { robots } from 'vite-plugin-robots'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    robots({
+      /* pass your config */
+    })
+  ]
 })
