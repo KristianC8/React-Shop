6002f7463c0d3cfe38cc390df2a9c7b36fd68f76
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-07-27 14:18:58 -0500

    delete robots config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { robots } from 'vite-plugin-robots'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    robots({
-      /* pass your config */
-      filePath: '.robots.production.txt'
-    })
-  ]
+  plugins: [react()]
 })
